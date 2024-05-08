2baca53ef6ea86a3732a296f6ab0704b535adcd3
Luis Barreto <luis.barreto.marin@gmail.com>
2024-05-07 20:36:21 -0400

    add working chat

--- app/src/main/java/com/barreto/exe/gochat/Activities/ChatActivity.java
+++ app/src/main/java/com/barreto/exe/gochat/Activities/ChatActivity.java
@@ -6,6 +6,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.barreto.exe.gochat.adapters.ChatAdapter;
+import com.barreto.exe.gochat.adapters.MessageAdapter;
 import com.barreto.exe.gochat.api.ApiHandler;
 import com.barreto.exe.gochat.databinding.ActivityChatBinding;
 import com.barreto.exe.gochat.models.Chat;
@@ -13,14 +15,21 @@ import com.barreto.exe.gochat.models.Configs;
 import com.barreto.exe.gochat.models.Message;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
 
     private ActivityChatBinding binding;
     private final ApiHandler apiHandler = new ApiHandler();
     private Chat chat;
+    private List<Message> messages;
+    private MessageAdapter messageAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,7 @@ public class ChatActivity extends AppCompatActivity {
         binding = ActivityChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        init();
         setListeners();
 
         chat = (Chat) getIntent().getSerializableExtra("chat");
@@ -36,6 +46,12 @@ public class ChatActivity extends AppCompatActivity {
             binding.titleTextView.setText(chat.getName());
             getChatMessages();
         }
+    }
+
+    void init(){
+        messages = new ArrayList<>();
+        messageAdapter = new MessageAdapter(messages, Configs.GetUserUuid(this));
+        binding.messagesRecyclerView.setAdapter(messageAdapter);
     }
 
     void setListeners(){
@@ -83,9 +99,30 @@ public class ChatActivity extends AppCompatActivity {
             apiHandler.getChatMessages(chatId, new ApiHandler.ApiCallback() {
                 @Override
                 public void onSuccess(Object data) {
-                    Message[] messages = (Message[]) data;
+                    Message[] newMessages = (Message[]) data;
+
+                    int count = messages.size();
+
+                    //Add new messages to the list
+                    messages.clear();
+                    messages.addAll(Arrays.asList(newMessages));
+
+                    //Sort messages by date
+                    messages.sort(Comparator.comparing(Message::getDateTime));
+
+                    //Notify the adapter
+                    if (count == 0)
+                    {
+                        messageAdapter.notifyDataSetChanged();
+                    }
+                    else
+                    {
+                        messageAdapter.notifyItemRangeInserted(messages.size(), messages.size());
+                        binding.messagesRecyclerView.smoothScrollToPosition(messages.size()-1);
+                    }
 
                     binding.progressBar.setVisibility(View.GONE);
+                    binding.messagesRecyclerView.setVisibility(View.VISIBLE);
                 }
 
                 @Override
--- app/src/main/java/com/barreto/exe/gochat/adapters/MessageAdapter.java
+++ app/src/main/java/com/barreto/exe/gochat/adapters/MessageAdapter.java
@@ -1,12 +1,66 @@
 package com.barreto.exe.gochat.adapters;
 
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.barreto.exe.gochat.databinding.ItemContainerReceivedMessageBinding;
 import com.barreto.exe.gochat.databinding.ItemContainerSentMessageBinding;
 import com.barreto.exe.gochat.models.Message;
 
-public class MessageAdapter {
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final List<Message> messages;
+    private final String userId;
+
+    public static final int VIEW_TYPE_SENT = 1;
+    public static final int VIEW_TYPE_RECEIVED = 2;
+
+    public MessageAdapter(List<Message> messages, String userId) {
+        this.messages = messages;
+        this.userId = userId;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if(viewType == VIEW_TYPE_SENT)
+        {
+            return new SentMessageViewHolder(ItemContainerSentMessageBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+        }
+
+        return new ReceivedMessageViewHolder(ItemContainerReceivedMessageBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(getItemViewType(position) == VIEW_TYPE_SENT)
+        {
+            ((SentMessageViewHolder) holder).setData(messages.get(position));
+        }
+        else
+        {
+            ((ReceivedMessageViewHolder) holder).setData(messages.get(position));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(messages.get(position).getSenderId().equals(userId)){
+            return VIEW_TYPE_SENT;
+        } else {
+            return VIEW_TYPE_RECEIVED;
+        }
+    }
+
     static class SentMessageViewHolder extends RecyclerView.ViewHolder {
         ItemContainerSentMessageBinding itemBinding;
         SentMessageViewHolder(ItemContainerSentMessageBinding itemBinding) {
@@ -33,6 +87,7 @@ public class MessageAdapter {
             itemBinding.textSender.setText(message.getSenderUsername());
             itemBinding.textTime.setText(message.getDateTime().toString());
         }
+
     }
 
 }
